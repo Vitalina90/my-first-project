e36c4129340df52b63d161faccb270cd04774905
Vitalina <vsinkovaa@gmail.com>
2025-02-07 13:27:23 +0200

    js

---
+++ js/task-practical.js
@@ -0,0 +1,10 @@
+// function cheakAge(age) {
+//   if (age < 18) {
+//     return "Access denied. You are too young.";
+//   } else if (age >= 18 && age < 65) {
+//     return "Access granted. Welcome!";
+//   } else {
+//     return "Access granted. Enjoy your senior benefits!";
+//   }
+// }
+// console.log(cheakAge(64));
